0c4e0d5cca61798c91327fadffc1521802a9395c
Seokjun.Kang <kangsj0327@gmail.com>
2025-11-28 01:23:05 +0900

    chore: jest 테스트 시 API 모킹을 위한 jest, msw 환경 구성 및 예시 API 핸들러 추가

--- jest.config.ts
+++ jest.config.ts
@@ -1,20 +1,15 @@
-/**
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
-
 import type { Config } from 'jest';
 import nextJest from 'next/jest.js';
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 });
 
 const config: Config = {
   coverageProvider: 'v8',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-fixed-jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/e2e/'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
 
 export default createJestConfig(config);
