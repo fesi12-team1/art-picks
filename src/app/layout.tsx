1eedd1fc5536053da78d6cdf68373f75ce1f541e
Seokjun.Kang <kangsj0327@gmail.com>
2025-12-01 14:24:53 +0900

    chore: browser 환경 msw 실행 파일 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from 'next';
-
 import './globals.css';
-
 import { initMocks } from '@/mocks';
 
 initMocks();
--- src/mocks/browser.ts
+++
@@ -1,5 +0,0 @@
-import { setupWorker } from 'msw/browser';
-
-import { postsHandlers } from '@/mocks/handlers/posts';
-
-export const worker = setupWorker(...postsHandlers);
