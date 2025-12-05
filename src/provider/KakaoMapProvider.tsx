5d45e53e1b6a13f8003b60644922d9356927ecdd
Seokjun.Kang <kangsj0327@gmail.com>
2025-12-05 16:11:35 +0900

    feat: kakao map을 보여주는 createmap 함수 추가

--- src/provider/KakaoMapProvider.tsx
+++ src/provider/KakaoMapProvider.tsx
@@ -4,10 +4,18 @@ import Script from 'next/script';
 import React, { createContext, useContext, useState } from 'react';
 
 const KAKAO_JS_KEY = process.env.NEXT_PUBLIC_KAKAO_JS_KEY;
-const KAKAO_MAP_SCRIPT_SRC = `${process.env.NEXT_PUBLIC_KAKAO_MAP_URL}${KAKAO_JS_KEY}`;
+const KAKAO_MAP_SCRIPT_SRC = `${process.env.NEXT_PUBLIC_KAKAO_MAP_URL}?appkey=${KAKAO_JS_KEY}&autoload=false`;
+
+type CreateMapFn = (
+  container: HTMLElement,
+  options: Omit<kakao.maps.MapOptions, 'center'> & {
+    center: { lat: number; lng: number };
+  }
+) => kakao.maps.Map | undefined;
 
 interface KakaoMapContextValue {
   loaded: boolean;
+  createMap: CreateMapFn;
 }
 
 const KakaoMapContext = createContext<KakaoMapContextValue | null>(null);
@@ -16,9 +24,27 @@ export function KakaoMapProvider({ children }: { children: React.ReactNode }) {
   const [loaded, setLoaded] = useState(false);
   const [, setError] = useState<Error | null>(null);
 
+  const createMap: CreateMapFn = (container, options) => {
+    if (!loaded || !window.kakao?.maps) {
+      return;
+    }
+
+    const mapOptions = {
+      center: new window.kakao.maps.LatLng(
+        options.center.lat,
+        options.center.lng
+      ),
+      level: options.level,
+    };
+
+    return new window.kakao.maps.Map(container, mapOptions);
+  };
+
   const handleLoad = () => {
-    if (window.kakao && window.kakao.maps) {
-      setLoaded(true);
+    if (window.kakao) {
+      window.kakao.maps.load(() => {
+        setLoaded(true);
+      });
     } else {
       setError(
         new Error(
@@ -48,7 +74,7 @@ export function KakaoMapProvider({ children }: { children: React.ReactNode }) {
         onLoad={() => handleLoad()}
         onError={() => handleError()}
       />
-      <KakaoMapContext.Provider value={{ loaded }}>
+      <KakaoMapContext.Provider value={{ loaded, createMap }}>
         {children}
       </KakaoMapContext.Provider>
     </>
