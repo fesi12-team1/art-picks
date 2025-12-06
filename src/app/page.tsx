e89e08f4f8141652665b2e180427d414fbdc61e7
Seokjun.Kang <kangsj0327@gmail.com>
2025-12-06 16:41:01 +0900

    chore: 홈페이지 스타일 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function HomePage() {
   return (
-    <main className="h-main flex-center">
+    <main className="h-main flex items-center justify-center">
       <p className="text-title1-semibold bg-brand-500 tablet:bg-brand-600 laptop:bg-brand-700 desktop:bg-gray-200 p-5">
         Hello, world!
       </p>
