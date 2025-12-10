fb508725c05053fbbaee215deb20d78cb6e22faf
Yun-Jinwoo <yjw3480@naver.com>
2025-12-10 15:41:11 +0900

    feat: 크루 멤버 수 조회 Hook useCrewMemberCount 구현

---
+++ src/hooks/queries/crews/useCrewMemberCount.ts
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
+import { getCrewMemberCount } from '@/api/crews';
+import { QUERY_KEYS } from '@/lib/constants';
+
+/**
+ * 크루 멤버 수 조회
+ * @param crewId
+ */
+
+export default function useCrewMemberCount(crewId: number) {
+  return useQuery({
+    queryKey: QUERY_KEYS.members.count(crewId),
+    queryFn: () => getCrewMemberCount(crewId),
+    enabled: !!crewId, // crewId가 유효할 때만 쿼리가 자동으로 실행
+  });
+}
