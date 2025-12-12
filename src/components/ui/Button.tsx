41b4758283637c49714a4fbac121d1f2da4fb714
Seokjun.Kang <kangsj0327@gmail.com>
2025-12-12 13:36:27 +0900

    fix: Button 컴포넌트의 스타일에서 불필요한 'text-sm' 클래스 제거, 높이 및 배경색 올바르게 수정

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,22 +4,22 @@ import * as React from 'react';
 import { cn } from '@/lib/utils';
 
 export const buttonVariants = cva(
-  "inline-flex shrink-0 items-center justify-center rounded-xl text-sm font-medium whitespace-nowrap transition-all outline-none disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+  "inline-flex shrink-0 items-center justify-center rounded-xl whitespace-nowrap transition-all outline-none disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
   {
     variants: {
       variant: {
         default:
           'bg-brand-500 focus-visible:bg-brand-600 hover:bg-brand-600 active:bg-brand-700 text-white hover:text-gray-50 focus-visible:text-gray-50 active:text-gray-200 disabled:bg-gray-700 disabled:text-gray-300',
         outlined:
-          'text-brand-400 hover:text-brand-600 focus-visible:text-brand-600 active:text-brand-700 border-brand-400 hover:border-brand-600 focus-visible:border-brand-600 active:border-brand-700 border bg-transparent active:bg-gray-800 disabled:border-gray-400 disabled:bg-gray-400 disabled:text-gray-400',
+          'text-brand-400 hover:text-brand-600 focus-visible:text-brand-600 active:text-brand-700 border-brand-400 hover:border-brand-600 focus-visible:border-brand-600 active:border-brand-700 border bg-transparent active:bg-gray-800 disabled:border-gray-400 disabled:text-gray-400',
         neutral:
           'disabled:bg-gray-750 bg-gray-400 text-white hover:bg-gray-600 hover:text-gray-200 focus-visible:bg-gray-600 focus-visible:text-gray-200 active:bg-gray-700 active:text-gray-300 disabled:text-gray-400',
         ghost:
           'bg-transparent hover:bg-gray-800 focus-visible:bg-gray-800 active:bg-gray-700 text-white hover:text-gray-200 focus-visible:text-gray-200 active:text-gray-300 disabled:text-gray-400',
       },
       size: {
-        default: 'text-body2-semibold px-6 py-3 has-[>svg]:px-3',
-        sm: 'text-body3-semibold px-6 py-2 has-[>svg]:px-2.5',
+        default: 'text-body2-semibold h-12 px-6 py-3 has-[>svg]:px-3',
+        sm: 'text-body3-semibold h-9 px-6 py-2 has-[>svg]:px-2.5',
       },
     },
     defaultVariants: {
