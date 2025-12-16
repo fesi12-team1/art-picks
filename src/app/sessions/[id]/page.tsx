2b1751cb37244e25c0bf7d111d9c12354789f074
Seokjun.Kang <kangsj0327@gmail.com>
2025-12-16 11:19:13 +0900

    feat: session detail 페이지에 데이터 페칭, 로딩, 에러 처리를 추가

--- src/app/sessions/[id]/page.tsx
+++ src/app/sessions/[id]/page.tsx
@@ -1,3 +1,24 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'next/navigation';
+import { sessionQueries } from '@/api/queries/sessionQueries';
+
 export default function Page() {
+  const { id } = useParams();
+  const {
+    data: session,
+    error,
+    isLoading,
+  } = useQuery(sessionQueries.detail(Number(id)));
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {(error as Error).message}</div>;
+  }
+
   return <div>page</div>;
 }
