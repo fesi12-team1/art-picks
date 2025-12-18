a16de5867068b11891a365053048d13f292a0c30
Yun-Jinwoo <yjw3480@naver.com>
2025-12-18 17:47:43 +0900

    feat: 마이페이지 레이아웃 구현

---
+++ src/app/my/layout.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import MyInfo from '@/components/my/MyInfo';
+import MyTabs from '@/components/my/MyTabs';
+import { useMediaQuery } from '@/hooks/useMediaQuery';
+
+export default function MyLayout({ children }: { children: React.ReactNode }) {
+  const isTablet = useMediaQuery('tablet');
+  const isMobile = useMediaQuery('mobile');
+  const isSmallDevice = isTablet || isMobile;
+
+  return (
+    <div className="tablet:pt-7.5 laptop:pt-[47px] tablet:mx-20 laptop:mx-auto mx-6 flex max-w-[1320px] gap-20 pt-3.5">
+      <aside className="laptop:block ml-5 hidden w-[327px]">
+        <MyInfo />
+      </aside>
+
+      <div className="tablet:gap-16 laptop:gap-10 mx-5 flex min-w-0 flex-1 flex-col gap-[27px]">
+        <div className="block w-full">
+          <MyTabs isSmallDevice={isSmallDevice} />
+        </div>
+        <main className="flex-1">{children}</main>
+      </div>
+    </div>
+  );
+}
