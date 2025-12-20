18dda2d56c19c5e37aa8b18070ce7312273624bd
Seokjun.Kang <kangsj0327@gmail.com>
2025-12-21 00:23:25 +0900

    feat: session creation page 기본 레이아웃 스타일링 및 뒤로가기 버튼 추가

--- src/app/sessions/create/page.tsx
+++ src/app/sessions/create/page.tsx
@@ -1,3 +1,37 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import ShevronLeft from '@/assets/icons/chevron-left.svg?react';
+
 export default function Page() {
-  return <div>page</div>;
+  return (
+    <main className="h-main laptop:my-[50px] laptop:py-0 laptop:px-8 mx-auto max-w-[1120px] p-6">
+      <div className="mb-6 flex items-center gap-2">
+        <BackButton />
+        <h1 className="text-body1-semibold laptop:text-title2-semibold">
+          세션 생성하기
+        </h1>
+      </div>
+      <SessionCreateForm />
+    </main>
+  );
+}
+
+export function BackButton() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    if (window.history.length > 1) router.back();
+    else router.replace('/sessions');
+  };
+
+  return <ShevronLeft type="button" onClick={handleClick} className="size-6" />;
+}
+
+function SessionCreateForm() {
+  return (
+    <div>
+      <h1>Create a New Session</h1>
+    </div>
+  );
 }
