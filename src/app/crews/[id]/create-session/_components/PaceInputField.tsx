335799fcc8d268a8e1614b7071d8d9288684b570
Seokjun.Kang <kangsj0327@gmail.com>
2025-12-28 22:19:31 +0900

    refactor:  PaceInputField가 controller 방식으로 동작하도록 변경

--- src/app/crews/[id]/create-session/_components/PaceInputField.tsx
+++ src/app/crews/[id]/create-session/_components/PaceInputField.tsx
@@ -1,12 +1,12 @@
-import { useFormContext } from 'react-hook-form';
+import { Controller, useFormContext } from 'react-hook-form';
 import PaceSlider from '@/components/ui/PaceSlider';
 import { SessionCreateFormValues } from '../_others/schema';
 
 export default function PaceInputField() {
-  const { getValues, setValue } = useFormContext<SessionCreateFormValues>();
+  const { control } = useFormContext<SessionCreateFormValues>();
 
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <label>
         <p className="text-caption-semibold tablet:text-body3-semibold text-gray-50">
           {'페이스 (분/km)'}
@@ -15,11 +15,18 @@ export default function PaceInputField() {
           세션에서 함께 달릴 기준 페이스를 선택해주세요!
         </p>
       </label>
-      <PaceSlider
-        value={getValues('pace')}
-        onValueChange={(nextPace) => {
-          setValue('pace', nextPace);
-        }}
+
+      <Controller
+        control={control}
+        name="pace"
+        render={({ field: { onChange, value } }) => (
+          <PaceSlider
+            value={value}
+            onValueChange={(nextPace) => {
+              onChange(nextPace);
+            }}
+          />
+        )}
       />
     </div>
   );
