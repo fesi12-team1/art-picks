f0b1d1fc60e2bcb517b85f11ed975687d24af758
Seokjun.Kang <kangsj0327@gmail.com>
2025-12-28 22:50:20 +0900

    refactor: add error handling for max participant count input

--- src/app/crews/[id]/create-session/_components/MaxParticipantsInputField.tsx
+++ src/app/crews/[id]/create-session/_components/MaxParticipantsInputField.tsx
@@ -3,11 +3,15 @@ import Input from '@/components/ui/Input';
 import { SessionCreateFormValues } from '../_others/schema';
 
 export default function MaxParticipantsInputField() {
-  const { register } = useFormContext<SessionCreateFormValues>();
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<SessionCreateFormValues>();
   return (
     <Input
       label="모집 정원"
       placeholder="최소 2인 이상 입력해주세요"
+      errorMessage={errors.maxParticipantCount?.message}
       {...register('maxParticipantCount', { valueAsNumber: true })}
     />
   );
