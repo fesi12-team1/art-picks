4509105cf3133d93aef9af82eb249a779a84aa89
Seokjun.Kang <kangsj0327@gmail.com>
2025-12-28 22:17:47 +0900

    feat: 레벨 선택을 radio group으로 변경하여 선택이 올바르게 되지 않는 문제 수정 + 접근성 향상

--- src/app/crews/[id]/create-session/_components/LevelInputField.tsx
+++ src/app/crews/[id]/create-session/_components/LevelInputField.tsx
@@ -1,44 +1,51 @@
-import { useFormContext } from 'react-hook-form';
+import * as RadioGroup from '@radix-ui/react-radio-group';
+import { Controller, useFormContext } from 'react-hook-form';
 import SessionLevelCard from '@/components/session/SessionLevelCard';
 import { SessionCreateFormValues } from '../_others/schema';
 
 export default function LevelInputField() {
-  const { watch, setValue } = useFormContext<SessionCreateFormValues>();
-  const value = watch('level');
+  const { control } = useFormContext<SessionCreateFormValues>();
 
   return (
-    <div>
-      <label className="mb-4 block">
+    <fieldset>
+      <legend className="mb-4 block">
         <p className="text-caption-semibold tablet:text-body3-semibold text-gray-50">
           난이도
         </p>
         <p className="tablet:text-body3-semibold text-caption-regular text-gray-300">
           페이스와 별개로, 이 세션의 체감 난이도를 선택해주세요
         </p>
-      </label>
-      <div className="flex flex-col gap-3">
-        <SessionLevelCard
-          level="BEGINNER"
-          checked={value === 'BEGINNER'}
-          onClick={() => {
-            setValue?.('level', 'BEGINNER');
-          }}
-        />
-        <SessionLevelCard
-          level="INTERMEDIATE"
-          checked={value === 'INTERMEDIATE'}
-          onClick={() => {
-            setValue?.('level', 'INTERMEDIATE');
-          }}
-        />
-        <SessionLevelCard
-          level="ADVANCED"
-          checked={value === 'ADVANCED'}
-          onClick={() => {
-            setValue?.('level', 'ADVANCED');
-          }}
-        />
-      </div>
-    </div>
+      </legend>
+
+      <Controller
+        control={control}
+        name="level"
+        render={({ field: { onChange, value } }) => (
+          <RadioGroup.Root
+            className="flex flex-col gap-3"
+            value={value}
+            onValueChange={(val) => {
+              onChange(val);
+            }}
+          >
+            <SessionLevelCard
+              level="BEGINNER"
+              value="BEGINNER"
+              checked={value === 'BEGINNER'}
+            />
+            <SessionLevelCard
+              level="INTERMEDIATE"
+              value="INTERMEDIATE"
+              checked={value === 'INTERMEDIATE'}
+            />
+            <SessionLevelCard
+              level="ADVANCED"
+              value="ADVANCED"
+              checked={value === 'ADVANCED'}
+            />
+          </RadioGroup.Root>
+        )}
+      />
+    </fieldset>
   );
 }
