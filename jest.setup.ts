a59d322aa818db3f6d0381fe87571b411bd81102
Shion <seoy1108@gmail.com>
2025-12-31 14:29:03 +0900

    chore: next/navigation 모킹 및 초기화 함수 추가

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,17 +1,13 @@
 import '@testing-library/jest-dom';
 import { jest } from '@jest/globals';
+import { clearAllNavigationMocks } from '@/mocks/nextNavigation';
 
 globalThis.jest = jest;
 
-// next/navigation mock
-export const pushMock = jest.fn();
-jest.mock('next/navigation', () => ({
-  useRouter: () => ({
-    push: pushMock,
-    replace: jest.fn(),
-    prefetch: jest.fn(),
-  }),
-}));
+// 모든 mock 초기화
+afterEach(() => {
+  clearAllNavigationMocks();
+});
 
 if (process.env.NEXT_PUBLIC_USE_MSW === 'true') {
   let server: Awaited<typeof import('@/mocks/jest')>['server'];
