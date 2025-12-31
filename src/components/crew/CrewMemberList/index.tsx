4d6bf5d6666f3b26d19f384ef80bba4b94441b6b
Junyeong Heo <hhjjyy1122@gmail.com>
2025-12-31 17:18:18 +0900

    feat: 참여자, 멤버 목록 모달 모바일 화면 반응형

--- src/components/crew/CrewMemberList/index.tsx
+++ src/components/crew/CrewMemberList/index.tsx
@@ -5,8 +5,10 @@ import {
   useLeaveCrew,
   useUpdateMemberRole,
 } from '@/api/mutations/crewMutations';
+import ChevronLeft from '@/assets/icons/chevron-left.svg?react';
 import Settings from '@/assets/icons/settings.svg?react';
 import VerticalEllipsis from '@/assets/icons/vertical-ellipsis.svg?react';
+import X from '@/assets/icons/x.svg';
 import { RoleBadge } from '@/components/ui/Badge';
 import Button from '@/components/ui/Button';
 import Dropdown from '@/components/ui/Dropdown';
@@ -59,18 +61,25 @@ export default function CrewMemberList({
           </Button>
         </Modal.Trigger>
         <Modal.Content
-          className="tablet:h-[620px] flex h-full w-[400px] flex-col bg-gray-800"
+          className="tablet:h-[620px] tablet:w-[400px] flex flex-col bg-gray-800"
+          fullscreenWhenMobile
           onCloseAutoFocus={() => setEditMode('view')}
         >
-          <Modal.Title className="self-start">
-            <div className="flex flex-col">
+          <Modal.Title className="relative flex w-full items-start gap-2 self-start">
+            <Modal.EmptyCloseButton
+              className="laptop:hidden my-0.5 flex"
+              onClick={() => setEditMode('view')}
+            >
+              <ChevronLeft className="size-6" />
+            </Modal.EmptyCloseButton>
+            <div className="flex flex-col gap-1">
               <div className="flex items-center gap-2">
-                <span className="text-title3-semibold line-clamp-1 text-gray-50">
+                <span className="text-body1-semibold line-clamp-1 text-white">
                   {crew.name}
                 </span>
                 {myRole === 'LEADER' && (
                   <Settings
-                    className="size-5 fill-gray-300"
+                    className="size-5 fill-gray-200"
                     onClick={() => setEditMode('edit')}
                   />
                 )}
@@ -79,8 +88,11 @@ export default function CrewMemberList({
                 {crew.city} • 멤버 {members.length}명
               </span>
             </div>
+            <Modal.CloseButton
+              className="laptop:flex absolute top-0 right-0 my-0.5 hidden"
+              onClick={() => setEditMode('view')}
+            />
           </Modal.Title>
-          <Modal.CloseButton onClick={() => setEditMode('view')} />
           <div className="h-0 self-stretch outline-1 outline-offset-[-0.50px] outline-gray-700" />
           <Modal.Description className="flex w-full flex-col overflow-y-scroll [&::-webkit-scrollbar]:w-1.5 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-400 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-800">
             {members.map((member) => (
