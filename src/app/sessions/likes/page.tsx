485e684e26c1180dc309ba3027650660b96297f2
Seokjun.Kang <kangsj0327@gmail.com>
2026-01-02 00:20:25 +0900

    feat: create SessionList component and integrate it into the liked sessions page

--- src/app/sessions/likes/page.tsx
+++ src/app/sessions/likes/page.tsx
@@ -1,20 +1,8 @@
-'use client';
-
-import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
-import { userQueries } from '@/api/queries/userQueries';
-import SessionCard from '@/components/session/SessionCard';
-import { useInfiniteScroll } from '@/hooks/useInfiniteScroll';
+import { ErrorBoundary, Suspense } from '@suspensive/react';
+import Spinner from '@/components/ui/Spinner';
+import SessionList from './_components/SessionList';
 
 export default function Page() {
-  const query = useSuspenseInfiniteQuery(userQueries.me.likes());
-  const loadMoreRef = useInfiniteScroll(
-    () => query.fetchNextPage(),
-    query.hasNextPage
-  );
-
-  if (query.isLoading) return <div>Loading...</div>;
-  if (query.isError) return <div>Failed</div>;
-
   return (
     <main className="h-main tablet:px-8 laptop:px-0 mx-auto w-full max-w-[1120px] px-4">
       <div className="tablet:block mt-[45px] mb-[43px] ml-1 hidden">
@@ -23,16 +11,11 @@ export default function Page() {
           마감되기 전에 지금 바로 참여해보세요!
         </p>
       </div>
-      <ul className="laptop:grid-cols-3 grid w-full grid-cols-2 gap-6">
-        {query.data?.sessions.map((session) => (
-          <SessionCard
-            key={session.id}
-            session={session}
-            displayParticipants={true}
-          />
-        ))}
-      </ul>
-      <div ref={loadMoreRef} className="h-1" />
+      <ErrorBoundary fallback={<div>세션 목록을 불러오는데 실패했습니다.</div>}>
+        <Suspense fallback={<Spinner />}>
+          <SessionList />
+        </Suspense>
+      </ErrorBoundary>
     </main>
   );
 }
